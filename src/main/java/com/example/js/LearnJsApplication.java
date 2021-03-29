2a6522af9eece71e31b0b344a6d280789fae7b44
songk1992-home <zuppiy@naver.com>
2021-03-29 21:49:32 +0900

    init

---
+++ src/main/java/com/example/js/LearnJsApplication.java
@@ -0,0 +1,13 @@
+package com.example.js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJsApplication.class, args);
+	}
+
+}
