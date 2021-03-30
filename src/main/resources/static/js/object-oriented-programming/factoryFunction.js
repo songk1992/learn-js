b36b40c52fdae4cf0e19958f3a96f72c5962d0d2
songk1992-home <zuppiy@naver.com>
2021-03-30 21:51:10 +0900

    OOP : ES6 Classes

--- src/main/resources/static/js/object-oriented-programming/factoryFunction.js
+++ src/main/resources/static/js/object-oriented-programming/factoryFunction.js
@@ -1,14 +1,18 @@
 
-function Ninja(name, weapon){
-	this.name = name;
-	this.weapon = weapon;
+class Ninja{
+	constructor(name, weapon){
+		this.name = name;
+		this.weapon = weapon;
+		}
+	
+	attack = function() {
+		return 'attack with' + this.weapon
+		}
 }
 
-Ninja.prototype.attack = function() {
-		return 'attack with' + this.weapon
-	}
 
 const gamja = new Ninja('감자', '칼')
 console.log(gamja.attack())
+console.log(gamja instanceof Ninja)
 const goguma = new Ninja('고구마', '도끼')
 console.log(goguma.attack())
