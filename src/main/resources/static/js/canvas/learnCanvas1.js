473728ac0aab303d51ac377a5c57def37621cf42
songk1992-home <zuppiy@naver.com>
2021-04-01 09:40:59 +0900

    saving image on canvas

--- src/main/resources/static/js/canvas/learnCanvas1.js
+++ src/main/resources/static/js/canvas/learnCanvas1.js
@@ -1,2 +1,23 @@
-let c = document.querySelector("#c");
-let ctx = c.getContext("2d");
+
+window.onload = function () {
+	
+var c = document.getElementById("mycanvas");
+console.log(c);
+var ctx = c.getContext("2d");
+
+var image = new Image();
+image.onload = function() {
+	console.log("Loaded image");
+	ctx.drawImage(image, 0, 0, c.width, c.height);
+	var savedImage = c.toDataURL();
+	window.open(savedImage);
+}
+
+
+var rootPath = window.location.protocol + "//" + window.location.host + "/";
+var path = window.location.pathname;
+rootPath = rootPath + "images/"
+let imagePath =  rootPath + "jesus.png"
+image.src = imagePath;
+
+}
