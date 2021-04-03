31d3959ebcb58dbc8225d10127e39200d24169f8
songk1992-home <zuppiy@naver.com>
2021-04-03 11:42:28 +0900

    캔버스 스터디

--- src/main/java/com/example/js/controller/LearnCanvasController.java
+++ src/main/java/com/example/js/controller/LearnCanvasController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LearnCanvasController {
-	@GetMapping("learn/canvas")
-	public String LearnCanvas() {
+	@GetMapping("learn/canvas1")
+	public String LearnCanvas1() {
 		return "view/canvas/learn-canvas1.html";
+	}
+	
+	@GetMapping("learn/canvas2")
+	public String LearnCanvas2() {
+		return "view/canvas/learn-canvas2.html";
 	}
 }
