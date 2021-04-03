817e7b6b8be9b930e76ee4521ed5b463c9b1be09
songk1992-home <zuppiy@naver.com>
2021-04-03 21:17:23 +0900

    Compose 를 이용해 Assembly 라인을 만들수 있음

---
+++ src/main/resources/static/js/functional_programming/compose.js
@@ -0,0 +1,8 @@
+const compose = (f, g) => (data) => f(g(data))
+const multiplyBy3 = (num) => num * 3;
+const makePositive = (num) => Math.abs(num);
+
+const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive)
+
+str = multiplyBy3AndAbsolute(-50);
+console.log(str);
