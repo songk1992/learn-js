2774802b994c55581c213f68056f8b4ea1c44b1d
songk1992-home <zuppiy@naver.com>
2021-04-03 17:18:08 +0900

    Imperative Declartive

--- src/main/resources/static/js/functional_programming/shoppingCart.js
+++ src/main/resources/static/js/functional_programming/shoppingCart.js
@@ -63,7 +63,11 @@ function notGood(num){
 
 console.log(notGood(5));
 
-
+//Imperative Declartive
+for(let i=0; i< 1000; i++){
+	console.log(i);
+}
+[1,2,3,4,5].forEach(item => console.log(item));
 
 
 
