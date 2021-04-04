69f2b0af0f481e28366bfacad8e21387bcca61a9
songk1992-home <zuppiy@naver.com>
2021-04-04 15:43:03 +0900

    실시간 마우스 위치 표시

---
+++ src/main/resources/static/js/async_programming/getMousePosition.js
@@ -0,0 +1,18 @@
+setTimeout(()=>{
+let elem = document.getElementById('para');
+	console.log(elem);
+	
+elem.addEventListener("click", event => {
+	console.log(event.pageX, event.pageY);
+});
+	
+var pageX = document.getElementById("x");
+var pageY = document.getElementById("y");
+
+elem.addEventListener('mousemove', event => {
+  pageX.innerText = event.pageX;
+  pageY.innerText = event.pageY;
+});
+
+
+}, 0);
