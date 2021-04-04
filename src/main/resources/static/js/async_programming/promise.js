e09ed83400b53786ac782dcc5d327e2fb5fa9b73
songk1992-home <zuppiy@naver.com>
2021-04-04 18:57:33 +0900

    aync await

--- src/main/resources/static/js/async_programming/promise.js
+++ src/main/resources/static/js/async_programming/promise.js
@@ -21,7 +21,8 @@ promise
 .catch(() => console.log('3차 error'))
 
 
-
+/////////////////////////////// 방법 1
+/*
 const urls = [
 	'https://jsonplaceholder.typicode.com/posts',
 	'https://jsonplaceholder.typicode.com/todos',
@@ -35,4 +36,37 @@ Promise.all(urls.map(url =>{
 	console.log(results[1])
 	console.log(results[2])
 }).catch(() => console.log('error'))
+*/
 
+//////////////////////////////////////// 방법2 Async Await (방법1 보다 최신 방법)
+
+// 하나
+async function fetchUsers(){
+	const resp = await fetch('http://jsonplaceholder.typicode.com/posts')
+	const data = await resp.json();
+	console.log(data);
+}
+
+fetchUsers();
+
+// 둘
+const urls = [
+	'https://jsonplaceholder.typicode.com/posts',
+	'https://jsonplaceholder.typicode.com/todos',
+	'https://jsonplaceholder.typicode.com/photos'
+]
+
+const getData = async function(){
+	
+	try{
+		const [posts, todos, photos] = await Promise.all(urls.map(url => fetch(url).then(resp=> resp.json()) ))
+		console.log('posts', posts)
+		console.log('todos', todos)
+		console.log('photos', photos)
+	} catch(error){
+		console.log('error in async function', error)
+	}
+
+}
+
+getData();
