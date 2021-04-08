638b7a0f123db4437204c425d82223737f707c55
songk1992-home <zuppiy@naver.com>
2021-04-08 09:22:19 +0900

    async 스터디

--- src/main/resources/static/js/n2021/async_programming/async.js
+++ src/main/resources/static/js/n2021/async_programming/async.js
@@ -6,6 +6,84 @@ setTimeout(()=>{
 }, 0);
 console.log(3);
 
+////////////////////////////////////////////////
+// https://www.youtube.com/watch?v=aoQSOZfz3vQ
+
+// async
+async function fetchNumber() {
+	return 's';
+}
+
+const user = fetchNubmer();
+console.log(user);
+const user = fetchUser
+
+// await
+function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getApple(){
+	await delay(3000);
+	return 'apple';
+}
+
+async function getBanana(){
+	await delay(3000);
+	return 'banana';
+}
+
+// 너무 복잡
+function pickFruits(){
+	return getApple()
+	.then(apple =>{
+		return getBanana().then(banana => `${apple} + ${banana}`);
+	});
+}
+
+pickFruits().then(console.log);
+
+
+
+// 개선 
+async function pickFruits2(){
+	const apple = await getApple();
+	const banana = await getBanana();
+	return `${apple} + ${banana}`;
+}
+
+pickFruits2().then(console.log);
+
+
+// 개선 2 병렬실행
+async function pickFruits3(){
+	const applePromise = getApple();
+	const bananaPromise = getBanana();
+		
+	const apple = await applePromise;
+	const banana = await bananaPromise;
+	return `${apple} + ${banana}`;
+}
+
+pickFruits2().then(console.log);
+
+// 개선 3 API 사용해서 개선2 개선
+function pickAllFruits(){
+	return Promise.all([getApple(), getBanana()]).then(fruits => fruits.join('+'));
+}
+pickAllFruits().then(console.log);
+
+// 먼저 값을 리턴하는 값을 가짐
+function pickOnlyOne(){
+	return Promise.race([getApple(), getBanana()]);
+}
+
+pickOnlyOne().then(console.log);
+
+
+
+
+
 
 // https://www.youtube.com/watch?v=s1vpVCrT8f4
 // 콜백 지옥 예시
