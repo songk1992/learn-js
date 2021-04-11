f82fdef464eab47bb40045f01a54c0403f993151
songk1992-home <zuppiy@naver.com>
2021-04-11 20:57:29 +0900

    카 레이싱 게임 초기화

--- src/main/java/com/example/js/controller/N2021Controller.java
+++ src/main/java/com/example/js/controller/N2021Controller.java
@@ -2,9 +2,11 @@ package com.example.js.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class LearnCanvasController {
+@RequestMapping("/n2021/")
+public class N2021Controller {
 	@GetMapping("learn/canvas1")
 	public String LearnCanvas1() {
 		return "view/n2021/canvas/learn_canvas1.html";
@@ -17,6 +19,11 @@ public class LearnCanvasController {
 	
 	@GetMapping("learn/module")
 	public String LearnModule() {
+		return "view/n2021/learn_module.html";
+	}
+	
+	@GetMapping("minigame/car-racer")
+	public String CarRacer() {
 		return "view/n2021/learn_module.html";
 	}
 }
